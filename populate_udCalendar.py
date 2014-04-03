3743abcbe29261d1912490bb768adb5a4dd85dd6
Jacob Lee <jacoblee93@gmail.com>
2014-04-03 19:40:04 -0400

    Model version 1 finished

--- populate_udCalendar.py
+++ populate_udCalendar.py
@@ -1,24 +1,28 @@
 import os
+from django.utils import timezone
 
 def populate():
     u1=add_user('lloyd69', 'lloyd', 'lee', 'entourage', 'lloyd@princeton.edu')
     u2=add_user('sskoular', 'sonia', 'skoularikis', 'summer14', 'sskoular@princeton.edu')
     u3=add_user('tank', 'franklyn', 'darnis', 'lift', 'fdarnis@princeton.edu')
     u4=add_user('poushy', 'alex', 'pouschine', 'cloister', 'apouschine@princeton.edu')
+    u5=add_user('jacoblee', 'jacob', 'lee', 'squash', 'jacoblee@princeton.edu')
 
     ud1=add_updoguser(u1, 1)
     ud2=add_updoguser(u2, 2)
     ud3=add_updoguser(u3, 3)
     ud4=add_updoguser(u4, 4)
+    ud5=add_updoguser(u5, 5)
 
-    add_updogfriend(ud1, u3)
-    add_updogfriend(ud3, u1)
-    add_updogfriend(ud3, u4)
-    add_updogfriend(ud4, u3)
+    ud1.add_friend(ud2)
+    ud1.add_friend(ud3)
+    ud1.add_friend(ud4)
+    ud2.add_friend(ud3)
 
+    p = add_pack('cos333', [ud1, ud2, ud3, ud4])
+    p.add_user(ud5)
 
-    add_pack('cos333', [ud1, ud2, ud3, ud4])
-
+    dt1 = add_downtime(ud1, timezone.now(), timezone.now())
 
 def add_user(username, firstname, lastname, password, email):
 
@@ -31,11 +35,6 @@ def add_updoguser(user, fbID):
     udUser = UpDogUser.objects.get_or_create(user=user, fbID=fbID)[0]
     return udUser
 
-def add_updogfriend(friend, person):
-
-    udFriend = UpDogFriend.objects.get_or_create(friend=friend, person=person)[0]
-    return udFriend
-
 def add_pack(name, udUsers):
 
     pack = Pack.objects.get_or_create(name=name)[0]
@@ -44,17 +43,12 @@ def add_pack(name, udUsers):
 
     return pack
 
-def add_downtime(user, startTime, endTime, seeingFriends):
-    downtime = Downtime.objects.get_or_create(user = user, startTime = startTime, endTime = endTime)[0]
-    for friend in seeingFriends:
-        downtime.updoguser_set.add(friend)
-
+def add_downtime(user, startTime, endTime):
+    downtime = Downtime.objects.get_or_create(owner=user, start_time=startTime, end_time=endTime)[0]
     return downtime
 
 def add_event(startTime, endTime, participants):
-    event = Event.objects.get_or_create(startTime=startTime, endTime=endTime)[0]
-    for individual in participants:
-        event.updoguser_set.add(individual)
+    event = Event.objects.get_or_create(start_time=startTime, end_time=endTime)[0]
     return event
 
 if __name__ == '__main__':
@@ -62,6 +56,6 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'updog.settings')
 
     from django.contrib.auth.models import User
-    from udCalendar.models import UpDogUser, UpDogFriend, Pack, Downtime, Event
+    from udCalendar.models import UpDogUser, Friendship, Pack, Downtime, Event
 
     populate()
--- udCalendar/models.py
+++ udCalendar/models.py
@@ -7,48 +7,88 @@ class UpDogUser(models.Model):
 
     user = models.OneToOneField(User)
     fbID = models.CharField(max_length=100, unique=True)
-    visibleDowntimes = models.ManyToManyField('Downtime')
-    events = models.ManyToManyField('Event')
+    visible_downtimes = models.ManyToManyField('Downtime', related_name='seeing_users')
+    events = models.ManyToManyField('Event', related_name='owners')
     packs = models.ManyToManyField('Pack')
+    friends = models.ManyToManyField('self', through='Friendship', symmetrical=False, related_name='friend_to+')
 
     def __unicode__(self):
         return self.user.username
 
-class UpDogFriend(models.Model):
+    def add_friend(self, person, symm=True):
+        friendship, created = Friendship.objects.get_or_create(
+            from_user=self,
+            to_user=person)
 
-    friend = models.ForeignKey(UpDogUser)
-    person = models.OneToOneField(User)
-    dateLastSeen = models.DateField(auto_now=True)
-    meetingCount = models.IntegerField(default=0)
+        if symm:
+            person.add_friend(self, False)
+
+        return friendship
+
+    def remove_friend(self, person, symm=True):
+        Friendship.objects.filter(
+            from_user=self,
+            to_user=person).delete()
+        if symm:
+            person.remove_friend(self, False)
+        return
+
+    def get_friends(self):
+        return self.friends.filter(
+            friend_to__from_user=self)
+
+    def get_downtimes_on_day(self, date):
+        return self.downtime_set.filter(
+            start_time__day=date.day)
+
+    def get_events_on_day(self, date):
+        return self.events.filter(
+            start_time__day=date.day)
+
+class Friendship(models.Model):
+
+    to_user = models.ForeignKey(UpDogUser, related_name='friend_to')
+    from_user = models.ForeignKey(UpDogUser, related_name='friend_of')
+    date_last_seen = models.DateField(auto_now=True)
+    meeting_count = models.IntegerField(default=0)
     
     def __unicode__(self):
-        return self.person.username
+        return "From %s to %s" % (self.from_user, self.to_user)
         
 class Pack(models.Model):
     
     name = models.CharField(max_length=100)
-    dateLastSeen = models.DateField(auto_now=True)
-    meetingCount = models.IntegerField(default=0)
+    date_last_seen = models.DateField(auto_now=True)
+    meeting_count = models.IntegerField(default=0)
     
+    def add_user(self, user):
+        return self.updoguser_set.add(user)
+
+    def remove_user(self, user):
+        return self.updoguser_set.remove(user)
+
+    def get_users(self):
+        return self.updoguser_set.all()
+
     def __unicode__(self):
         return self.name
     
 class Downtime(models.Model):
 
-    user = models.ForeignKey(UpDogUser)
-    startTime = models.DateTimeField()
-    endTime = models.DateTimeField()
-    preferredActivity = models.CharField(max_length=100, null=True, blank=True)
+    owner = models.ForeignKey(UpDogUser)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    preferred_activity = models.CharField(max_length=100, null=True, blank=True)
 
-    def __unicode(self):
-        return self.startTime.strftime("%Y-%m-%d %H:%M:%S")
+    def __unicode__(self):
+        return self.start_time.strftime("%Y-%m-%d %H:%M:%S")
 
 class Event(models.Model):
 
-    startTime = models.DateTimeField()
-    endTime = models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     activity = models.CharField(max_length=100, null=True, blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
 
     def __unicode__(self):
-        return self.startTime.strftime("%Y-%m-%d %H:%M:%S")
+        return self.start_time.strftime("%Y-%m-%d %H:%M:%S")
