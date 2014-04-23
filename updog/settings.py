d0c2444d6c1f49756246de76ec878bc18e5f3d11
Jacob Lee <jacoblee93@gmail.com>
2014-04-23 01:47:59 -0400

    Heroku push

--- updog/settings.py
+++ updog/settings.py
@@ -124,11 +124,11 @@ DATABASES = {
 
 # Heroku database
 import dj_database_url
-#DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
