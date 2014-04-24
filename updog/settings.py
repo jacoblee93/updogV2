4e574009e580178038173aae6a75255d1479b451
Sonia Skoularikis <sskoularikis@nat-oitwireless-inside-vapornet100-b-32534.princeton.edu>
2014-04-24 03:30:55 -0400

    nothing

--- updog/settings.py
+++ updog/settings.py
@@ -123,7 +123,7 @@ DATABASES = {
 }
 
 # Heroku database
-import dj_database_url
+#import dj_database_url
 #DATABASES['default'] = dj_database_url.config()
 
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
