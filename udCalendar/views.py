6232a49082dc1ff753fc0f5f94180ccac68cc03e
Alex Pouschine <a.pouschine@gmail.com>
2014-04-25 13:36:59 -0400

    got rid of notifications testcode

--- udCalendar/views.py
+++ udCalendar/views.py
@@ -53,14 +53,14 @@ def calendar(request):
     #current_user.add_friend(UpDogUser.objects.order_by('-user')[3])
     current_user.add_friend(UpDogUser.objects.order_by('-user')[4])
 
-    test_to_friendship = Friendship.objects.filter(to_user=UpDogUser.objects.order_by('-user')[4], from_user=current_user)[0]
-    test_from_friendship = Friendship.objects.filter(from_user=UpDogUser.objects.order_by('-user')[4], to_user=current_user)[0]
-    test_to_friendship.is_mutual= True
-    test_from_friendship.is_mutual = True
-    test_to_friendship.is_new = False
-    test_from_friendship.is_new = False
-    test_to_friendship.save()
-    test_from_friendship.save()
+    #test_to_friendship = Friendship.objects.filter(to_user=UpDogUser.objects.order_by('-user')[4], from_user=current_user)[0]
+    #test_from_friendship = Friendship.objects.filter(from_user=UpDogUser.objects.order_by('-user')[4], to_user=current_user)[0]
+    #test_to_friendship.is_mutual= True
+    #test_from_friendship.is_mutual = True
+    #test_to_friendship.is_new = False
+    #test_from_friendship.is_new = False
+    #test_to_friendship.save()
+    #test_from_friendship.save()
 
     # Alex - friend request to build notifications bar
     #test_to_request = Friendship.objects.filter(to_user=UpDogUser.objects.order_by('-user')[2], from_user=current_user)[0]
@@ -72,7 +72,7 @@ def calendar(request):
     #test_from_request.is_mutual = False
     #test_from_request.save()
     #test_to_request.save()
-    #urrent_user.save()
+    #current_user.save()
 
     ships_list = current_user.get_friends()
 
