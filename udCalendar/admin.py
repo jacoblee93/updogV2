a2ccfd7e79bc9be699d2b0b7986b8aa8620a08cb
Jacob Lee <jacoblee93@gmail.com>
2014-04-27 01:58:39 -0400

    admin add

--- udCalendar/admin.py
+++ udCalendar/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from udCalendar.models import UpDogUser, Friendship, Pack, Downtime, Event
+from udCalendar.models import UpDogUser, Friendship, Pack, Downtime, Event, EventNotification
 
 # Register your models here.
 admin.site.register(UpDogUser)
@@ -7,3 +7,4 @@ admin.site.register(Friendship)
 admin.site.register(Pack)
 admin.site.register(Downtime)
 admin.site.register(Event)
+admin.site.register(EventNotification)
